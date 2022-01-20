c5bc0a5dbeadc0e9be590a6311926704e34722b7
RaghadAbdulhadi <raghadabdulhadi1999@gmail.com>
2022-01-20 23:32:51 +0200

    add some js popouts

--- Demo/js/app.js
+++ Demo/js/app.js
@@ -1,5 +1,25 @@
 'use strict';
+var name = prompt('Please enter your Name');
+var gender = prompt('What is your Gender (Male or Female)?')
 
 
+var age = prompt('What is your age?');
+if (age > 0) {
+    var welcoming = confirm('Do you want to skip the welcoming message?')
+    if (welcoming == false) {
+        if (gender == 'Male' || gender == 'male') {
+            alert('Welcome to Tick your Task website Mr.' + name)
+        }
+        else if (gender == 'Female' || gender == 'female') {
+            alert('Welcome to Tick your Task website Ms.' + name)
+        }
+        else {
+            alert('Welcome to Tick your Task website')
+        }
+    }
+}
+else {
+    alert('Your age is not acceptable, Please make sure you entered your age correctly!')
+}
 
 
